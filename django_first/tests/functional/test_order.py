e68815cf8418b6c23ac500719b0e1a5be7aa67a8
Nazira17 <you@example.com>
2019-04-19 16:58:06 +0600

    Add conftest.py where data-fixture, change test_views.py, test_order.py, add request.user in hello.html

--- django_first/tests/functional/test_order.py
+++ django_first/tests/functional/test_order.py
@@ -1,56 +1,7 @@
 import pytest
-from django_first.models import Order, OrderItem, City, Location
-from django_first.models import Product, Store, StoreItem, Customer, Payment
+from django_first.models import Payment, City
 from django_first.exceptions import PaymentException, StoreException
 from django_first.exceptions import LocationException
-from django.contrib.auth.models import User
-
-
-@pytest.fixture
-def data():
-    product = Product.objects.create(
-        name='apple',
-        price=10
-    )
-    city = City.objects.create(
-        name='Almaty'
-    )
-    location = Location.objects.create(
-        city=city,
-        address='Abay str'
-    )
-    user = User.objects.create_user(
-        username='alice',
-        password='alice'
-    )
-    customer = Customer.objects.create(
-        name='Alice',
-        user=user
-    )
-    store = Store.objects.create(
-        location=location
-    )
-    store_item = StoreItem.objects.create(
-        store=store,
-        product=product,
-        quantity=100
-    )
-    order = Order.objects.create(
-        city=city,
-        customer=customer
-    )
-    order_item = OrderItem.objects.create(
-        order=order,
-        product=product,
-        quantity=10
-    )
-    payment = Payment.objects.create(
-        order=order,
-        amount=1000,
-        is_confirmed=True
-    )
-    return (product, customer, store, store_item,
-            order, order_item, payment, city, location)
 
 
 def test_order_process_ok(db, data):
