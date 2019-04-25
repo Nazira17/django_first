2ddee6e9ffab4637901f6e0179c848e46f4a3d6b
Nazira17 <you@example.com>
2019-04-25 17:03:15 +0600

    Add order list view Add order item post save signal

--- django_first/tests/functional/test_order.py
+++ django_first/tests/functional/test_order.py
@@ -1,5 +1,5 @@
 import pytest
-from django_first.models import Payment, City
+from django_first.models import Payment, City, OrderItem
 from django_first.exceptions import PaymentException, StoreException
 from django_first.exceptions import LocationException
 
@@ -7,13 +7,21 @@ from django_first.exceptions import LocationException
 def test_order_process_ok(db, data):
     (product, customer, store, store_item,
         order, order_item, payment, city, location) = data
+    assert order.price == 100
     order.process()
     store_item.refresh_from_db()
-    assert order.price == 100
     assert order.is_paid is True
     assert store_item.quantity == 90
     assert order.customer.name == 'Alice'
     assert order.customer.user.username == 'alice'
+
+
+def test_order_item_price_signal_ok(db, data):
+    (product, customer, store, store_item,
+        order, order_item, payment, city, location) = data
+    assert order.price == 100
+    OrderItem.objects.create(order=order, product=product, quantity=20)
+    assert order.price == 300
 
 
 def test_order_process_ok_mulitple_payments(db, data):
--- django_first/tests/functional/test_views.py
+++ django_first/tests/functional/test_views.py
@@ -1,3 +1,8 @@
+from lxml import html
+
+from django_first.models import Order
+
+
 def test_hello(db, client, data):
     client.login(username='alice', password='alice')
     response = client.get('/')
@@ -5,16 +10,12 @@ def test_hello(db, client, data):
     response = response.content.decode('utf-8')
     assert 'Hello, world!' in response
     assert 'alice' in response
+    response = html.fromstring(response)
+    orders = Order.objects.filter(customer__user__username='alice')
+    assert len(response.cssselect('li')) == orders.count()
 
 
 def test_bye(client):
     response = client.get('/bye/')
     assert response.status_code == 200
     assert response.content == b'Bye, world!'
-
-
-def test_third(client):
-    response = client.get('/third/')
-    assert response.status_code == 200
-    response = response.content.decode('utf-8')
-    assert 'Third' in response
