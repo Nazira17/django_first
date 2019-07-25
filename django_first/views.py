233992814d63d1db25bc1e48305296c1bc8463f1
Nazira17 <you@example.com>
2019-07-25 18:05:07 +0600

    Update test order view test to use cssselect and check for quantity

--- django_first/views.py
+++ django_first/views.py
@@ -1,25 +1,34 @@
-from django.http import HttpResponse
+from lxml import html
 from django.shortcuts import render
 
 from .models import Order
 
 
-def hello(request):
-    orders = Order.objects.filter(customer__user=request.user)
-    return render(request, 'hello.html', context={
-        'orders': orders
-    })
+def test_hello(db, client, data):
+    client.login(username='alice', password='alice')
+    response = client.get('/')
+    assert response.status_code == 200
+    response = response.content.decode('utf-8')
+    assert 'Hello, world!' in response
+    assert 'alice' in response
+    response = html.fromstring(response)
+    orders = Order.objects.filter(customer__user__username='alice')
+    assert len(response.cssselect('li')) == orders.count()
 
 
-def order(request, order_id):
-    order = Order.objects.get(id=order_id)
-    return render(request, 'order.html', context={
-        'order': order
-    })
+def test_order_view(db, client, data):
+    response = client.get('/orders/1/')
+    assert response.status_code == 200
+    response = response.content.decode('utf-8')
+    response = html.fromstring(response)
+    items = response.cssselect('.list-group-item')
+    assert items[0].text == 'apple 10'
 
 
-def bye(request):
-    return HttpResponse('Bye, world!')
+def test_bye(client):
+    response = client.get('/bye/')
+    assert response.status_code == 200
+    assert response.content == b'Bye, world!'
 
 
 def third(request):
